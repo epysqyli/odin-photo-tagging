52a4acf0ac0b6ad2bc61b09f7b7133cd68c7a4f4
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-08-23 18:21:44 +0200

    Add other characters to the homepage

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -6,6 +6,11 @@ const Home = (props) => {
   const waldoOne = props.waldoOne;
   const waldoTwo = props.waldoTwo;
   const waldoThree = props.waldoThree;
+  const wenda = props.wenda;
+  const odlaw = props.odlaw;
+  const wizard = props.wizard;
+  const woof = props.woof;
+
   return (
     <div className="home-container">
       <div className="title">where's waldo?</div>
@@ -14,9 +19,26 @@ const Home = (props) => {
         friends') stupid face among hundreds of seemingly identical faces. What
         fun!
         <div className="profile-images">
+          <h2>This is Waldo</h2>
           <img src={waldoTwo} alt="waldo profile pic" />
           <img src={waldoOne} alt="waldo profile pic" />
           <img src={waldoThree} alt="waldo profile pic" />
+        </div>
+        <div className="profile-images">
+          <h2>This is Odlaw</h2>
+          <img src={odlaw} alt="odlaw profile pic" />
+        </div>
+        <div className="profile-images">
+          <h2>This is Wenda</h2>
+          <img src={wenda} alt="wenda profile pic" />
+        </div>
+        <div className="profile-images">
+          <h2>This is Wizard Whitehead</h2>
+          <img src={wizard} alt="wizard profile pic" />
+        </div>
+        <div className="profile-images">
+          <h2>And then there's Woof</h2>
+          <img src={woof} alt="woof profile pic" />
         </div>
       </div>
       <a className="btn" href={gamePath}>
