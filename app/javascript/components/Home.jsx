61b94f74a4b8941ef6887f1f80255305b0daa92c
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-08-23 21:24:58 +0200

    Add separators between character names

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -15,10 +15,10 @@ const Home = (props) => {
         </p>
         <img src={charactersImg} alt="where's waldo game characters" />
         <div className="character-names">
-          <h3>Odlaw</h3>
-          <h3>Wizard Whitebeard</h3>
-          <h3>Wenda</h3>
-          <h3>Woof</h3>
+          <h3>Odlaw |</h3>
+          <h3>Wizard Whitebeard |</h3>
+          <h3>Wenda |</h3>
+          <h3>Woof |</h3>
           <h3>Waldo</h3>
         </div>
       </div>
