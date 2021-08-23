55ea84114d522c45f07ff50a21c5114290fad420
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-08-23 23:38:17 +0200

    Add picture dimension detection on component load

--- app/javascript/components/Picture.jsx
+++ app/javascript/components/Picture.jsx
@@ -2,13 +2,23 @@ import React, { useState, useEffect } from "react";
 
 const Picture = (props) => {
   const imagePath = props.imagePath;
+  const [dims, setDims] = useState({ width: null, height: null });
+
+  useEffect(() => {
+    const container = [...document.getElementsByClassName("container")][0];
+    const width = container.clientWidth;
+    const height = container.clientHeight;
+    let newDims = { ...dims };
+    newDims.width = width;
+    newDims.height = height;
+    setDims(newDims);
+  }, []);
+
   return (
     <div className="picture-container">
       <div className="container">
         <img src={imagePath} alt="waldo picture" />
-        <div className="example">
-          {/* put squares in here */}
-        </div>
+        <div className="example"></div>
       </div>
     </div>
   );
