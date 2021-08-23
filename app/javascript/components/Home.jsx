ccf2890ba3d2030a492917ca0cb76eae05c2206c
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-08-23 19:49:38 +0200

    Add character names on the homepage

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -2,17 +2,24 @@ import React from "react";
 
 const Home = (props) => {
   const gamePath = props.gamePath;
-  const charactersImg = props.charactersImg
+  const charactersImg = props.charactersImg;
 
   return (
     <div className="home-container">
       <div className="title">where's waldo?</div>
       <div className="description">
-        The game that will test your abilities to distinguish Waldo's (and his
-        friends') stupid face among hundreds of seemingly identical faces. What
-        fun!
-        <div className="characters">
-          <img src={charactersImg} alt="where's waldo game characters" />
+        <p>
+          The game that will test your abilities to distinguish Waldo's (and his
+          friends') stupid face among hundreds of seemingly identical faces.
+          What fun!
+        </p>
+        <img src={charactersImg} alt="where's waldo game characters" />
+        <div className="character-names">
+          <h3>Odlaw</h3>
+          <h3>Wizard Whitebeard</h3>
+          <h3>Wenda</h3>
+          <h3>Woof</h3>
+          <h3>Waldo</h3>
         </div>
       </div>
       <a className="btn" href={gamePath}>
