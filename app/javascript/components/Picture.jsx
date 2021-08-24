5d16e7a32466f0012672aad61a1a392fae27e4a3
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-08-24 14:20:24 +0200

    Add show menu

--- app/javascript/components/Picture.jsx
+++ app/javascript/components/Picture.jsx
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from "react";
 import Square from "../components/Square";
 
 const Picture = (props) => {
+  const [showMenu, setShowMenu] = useState({ show: false, squareNumber: null });
+  const [currentSquare, setCurrentSquare] = useState(null);
+
+  const handleClick = (squareNumber) => {
+    console.log(squareNumber);
+    setCurrentSquare(squareNumber);
+    setShowMenu({ show: true, squareNumber });
+  };
+
   const imagePath = props.imagePath;
 
   return (
@@ -10,7 +19,14 @@ const Picture = (props) => {
         <img src={imagePath} alt="waldo picture" />
         <div className="squares-container">
           {[...Array(1160)].map((element, index) => {
-            return <Square key={index} index={index} />;
+            return (
+              <Square
+                key={index}
+                index={index}
+                handleClick={handleClick}
+                showMenu={showMenu}
+              />
+            );
           })}
         </div>
       </div>
--- app/javascript/components/Square.jsx
+++ app/javascript/components/Square.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Square = (props) => {
-  const index = props.index;
-  return <div className="square" onClick={() => console.log(index)}></div>;
+  const popupMenu = (
+    <div className="popup-menu">
+      <div className="character-name">Waldo</div>
+      <div className="character-name">Wenda</div>
+      <div className="character-name">Odlaw</div>
+      <div className="character-name">Wizard Whitebeard</div>
+    </div>
+  );
+
+  const { index, handleClick, showMenu } = props;
+
+  return (
+    <div className="square" onClick={() => handleClick(index)}>
+      {showMenu.show && showMenu.squareNumber == index ? popupMenu : ""}
+    </div>
+  );
 };
 
 export default Square;
