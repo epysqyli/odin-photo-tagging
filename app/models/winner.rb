8a0786fc8a1b5995377e49ee2eab95968c4d68ed
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-08-26 20:34:51 +0200

    Remove leaderboard component

--- app/models/winner.rb
+++ app/models/winner.rb
@@ -1,4 +1,4 @@
 class Winner < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 25 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates :time, presence: true
 end
